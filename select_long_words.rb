861a48647c65caf6c440b7a08a4979bbd6a6adb8
Albert Ramos <albertramos.dev@gmail.com>
2021-11-16 11:41:11 -0700

    Add solution

--- select_long_words.rb
+++ select_long_words.rb
@@ -2,7 +2,7 @@
 # Write a method select_long_words(words) that takes in an array of words and returns a new array containing all of the words of the original array that are longer than 4 characters.
 
 def select_long_words(words)
-
+  words.select { |word| word if word.size > 4 }
 end
 
 print select_long_words(["what", "are", "we", "eating", "for", "dinner"]) # => ["eating", "dinner"]
