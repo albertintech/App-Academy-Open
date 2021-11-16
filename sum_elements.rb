d7671480cda88ac8845947ce17b9b6c6017cf7f1
Albert Ramos <albertramos.dev@gmail.com>
2021-11-16 11:59:06 -0700

    Add solution

--- sum_elements.rb
+++ sum_elements.rb
@@ -2,7 +2,15 @@
 # Write a method sum_elements(arr1, arr2) that takes in two arrays. The method should return a new array containing the results of adding together corresponding elements of the original arrays. You can assume the arrays have the same length.
 
 def sum_elements(arr1, arr2)
+  sum_arr = []
+  i = 0
 
+  while i < arr1.size
+    sum_arr << arr1[i] + arr2[i]
+    i += 1
+  end
+
+  sum_arr
 end
 
 print sum_elements([7, 4, 4], [3, 2, 11])
