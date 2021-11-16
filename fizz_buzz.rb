35c36e082258b57abaeaf132cbe15b1d494d1504
Albert Ramos <albertramos.dev@gmail.com>
2021-11-16 15:44:50 -0700

    Add solution

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -2,7 +2,18 @@
 # Write a method fizz_buzz(max) that takes in a number max and returns an array containing all numbers greater than 0 and less than max that are divisible by either 4 or 6, but not both.
 
 def fizz_buzz(max)
+  arr = []
+  i = 4
 
+  while i < max
+    unless i % 4 == 0 && i % 6 == 0
+      if i % 4 == 0 || i % 6 == 0
+        arr << i
+      end
+    end
+    i += 1
+  end
+  arr
 end
 
 print fizz_buzz(20) # => [4, 6, 8, 16, 18]
