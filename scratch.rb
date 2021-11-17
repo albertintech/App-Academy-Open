b49d4ab00353a21fb83b70e5849cb9ff469cb236
Albert Ramos <albertramos.dev@gmail.com>
2021-11-17 11:19:47 -0700

    Update file

---
+++ scratch.rb
@@ -0,0 +1,13 @@
+str = "follow the yellow brick road"
+
+chars = str.split("")
+p chars
+
+arr = %w(Hello world how are you)
+
+p arr
+
+p arr.join(" ") + "?"
+
+stringy = "Would you likes to rides the shoopuff?"
+print stringy.split("s").join("z")
