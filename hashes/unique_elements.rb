196e45528b56573c2cebf37759eacd9b6e4af85f
Albert Ramos <albertramos.dev@gmail.com>
2021-11-20 12:51:13 -0700

    Add solution

--- hashes/unique_elements.rb
+++ hashes/unique_elements.rb
@@ -3,8 +3,16 @@
 
 # Hint: all keys of a hash are automatically unique
 
-def unique_elements(arr)
+# The most Ruby way ignoring the "must use a hash" requirement
+# def unique_elements(arr)
+#   hsh.uniq
+# end
 
+# Ok. Now with the hash requirement
+def unique_elements(arr)
+  hsh = Hash.new(0)
+  arr.each { |ele| hsh[ele] = true }
+  return hsh.keys
 end
 
 print unique_elements(['a', 'b', 'a', 'a', 'b', 'c']) #=> ["a", "b", "c"]
