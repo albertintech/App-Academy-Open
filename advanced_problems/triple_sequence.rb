d513fa87b4249cebeec0a6745d65f2c62fadb8d7
Albert Ramos <albertramos.dev@gmail.com>
2021-11-23 11:59:07 -0700

    Add solution

--- advanced_problems/triple_sequence.rb
+++ advanced_problems/triple_sequence.rb
@@ -2,7 +2,14 @@
 # Write a method triple_sequence that takes in two numbers, start and length. The method should return an array representing a sequence that begins with start and is length elements long. In the sequence, every element should be 3 times the previous element. Assume that the length is at least 1.
 
 def triple_sequence(start, length)
-
+  arr = []
+  i = 0
+  while i < length
+    arr << start
+    start *= 3
+    i += 1
+  end
+  arr
 end
 
 print triple_sequence(2, 4) # => [2, 6, 18, 54]
