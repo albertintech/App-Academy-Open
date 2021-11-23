46a3c28012e83ef05fa123fafdacc2b7a276a017
Albert Ramos <albertramos.dev@gmail.com>
2021-11-23 11:36:40 -0700

    Add solution

--- advanced_problems/perfect_square.rb
+++ advanced_problems/perfect_square.rb
@@ -2,7 +2,13 @@
 # Write a method perfect_square? that takes in a number and returns a boolean indicating whether it is a perfect square. A perfect square is a number that results from multiplying a number by itself. For example, 9 is a perfect square because 3  3 = 9, 25 is a perfect square because 5  5 = 25.
 
 def perfect_square?(num)
+  (1..num).each do |i|
+    if i * i == num
+      return true
+    end
+  end
 
+  return false
 end
 
 puts perfect_square?(5)   #=> false
@@ -10,3 +16,11 @@ puts perfect_square?(12)  #=> false
 puts perfect_square?(30)  #=> false
 puts perfect_square?(9)   #=> true
 puts perfect_square?(25)  #=> true
+
+#Restate
+# -write method called perfect_square?
+#  -so predicate method that will return either true or false
+# -input an integer
+# -output boolean (true or false)
+# -result is we know if input number is a perfect square
+# -a perfect square is a number that results from a single number multiplied by itself, e.g. 25 is a perfect square becuase 5 x 5 = 25
