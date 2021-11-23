42ada7867abf18e1c17acfb909dd8d1d15caa5b6
Albert Ramos <albertramos.dev@gmail.com>
2021-11-23 16:25:31 -0700

    Add solution

--- advanced_problems/caesar_cipher.rb
+++ advanced_problems/caesar_cipher.rb
@@ -4,8 +4,17 @@
 # Feel free to use this variable:
 # alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-def caesar_cipher(str, num)
+def caesar_cipher(str, increment)
+  alphabet = "abcdefghijklmnopqrstuvwxyz" # index numbers 0-25
+  new_str = ""
 
+  str.each_char do |char| # iterate through each char in string
+    old_index = alphabet.index(char) # return the index of each char
+    new_index = old_index + increment # add increment value to each index
+    new_char = alphabet[new_index % 26] # use new index value mod 26 to find corresponding new char. Mod 26 allows us to "loop back" if new index goes over 25
+    new_str += new_char # concat new chars into new string
+  end
+  return new_str
 end
 
 puts caesar_cipher("apple", 1)    #=> "bqqmf"
