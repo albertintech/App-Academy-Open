7516c6635aa6740f507dfd2f80512982ebdf9dde
Albert Ramos <albertramos.dev@gmail.com>
2021-11-23 15:12:46 -0700

    Add solution

--- advanced_problems/fibonacci.rb
+++ advanced_problems/fibonacci.rb
@@ -1,9 +1,32 @@
 # Fibonacci
-# The fibonacci sequence is a sequence of numbers whose first and second elements are 1. To generate further elements of the sequence we take the sum of the previous two elements. For example the first 6 fibonacci numbers are 1, 1, 2, 3, 5, 8. Write a method fibonacci that takes in a number length and returns the fibonacci sequence up to the given length.
+# The fibonacci sequence is a sequence of numbers whose first and second elements are 1.
+# To generate further elements of the sequence we take the sum of the previous two elements. For example the first 6 fibonacci numbers are 1, 1, 2, 3, 5, 8.
+# Write a method fibonacci that takes in a number length and returns the fibonacci sequence up to the given length.
 
 def fibonacci(length)
-
+  arr = []
+  if length == 0
+    return arr
+  elsif length == 1
+    return arr << 1
+  else
+    arr = [1,1]
+    while arr.length < length
+      arr << arr[-2] + arr[-1]
+    end
+  end
+  return arr
 end
+
+# def summation(num)
+#   sum = 0
+#   count = 0
+#   while count <= num
+#     sum += count
+#     count += 1
+#   end
+#   return sum
+# end
 
 print fibonacci(0) # => []
 puts
