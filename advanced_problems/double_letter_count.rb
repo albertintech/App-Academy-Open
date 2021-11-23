afd054941e7767453cf95a1be241d299ca71297a
Albert Ramos <albertramos.dev@gmail.com>
2021-11-22 17:12:21 -0700

    Add exercise

---
+++ advanced_problems/double_letter_count.rb
@@ -0,0 +1,9 @@
+# Double Letter Count
+# Write a method that takes in a string and returns the number of times that the same letter repeats twice in a row.
+
+def double_letter_count(string)
+
+end
+
+puts double_letter_count("the jeep rolled down the hill") #=> 3
+puts double_letter_count("bootcamp") #=> 1
