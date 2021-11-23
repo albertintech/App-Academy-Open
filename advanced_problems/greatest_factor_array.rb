3662bd15fec731c2400ff324a6858f76f744e908
Albert Ramos <albertramos.dev@gmail.com>
2021-11-23 11:14:59 -0700

    Refactor

--- advanced_problems/greatest_factor_array.rb
+++ advanced_problems/greatest_factor_array.rb
@@ -2,33 +2,55 @@
 
 # Write a method greatest_factor_array that takes in an array of numbers and returns a new array where every even number is replaced with it's greatest factor. A greatest factor is the largest number that divides another with no remainder. For example the greatest factor of 16 is 8. (For the purpose of this problem we won't say the greatest factor of 16 is 16, because that would be too easy, ha)
 
+# def greatest_factor_array(arr)
+#   last_arr = []
+#   last_arr = consume_arr(arr)
+#   last_arr.map { |sub_arr| sub_arr.max(1) }
+# end
+#
+
+# Refactor: eliminate consume_arr method, move even number logic
 def greatest_factor_array(arr)
-  last_arr = []
-  last_arr = consume_arr(arr)
-  last_arr.map { |sub_arr| sub_arr.max(1) }
-end
-
-def factors(num)
-factors = []
-if num % 2 == 0
-   (1...num).each do |divisor|
-     if num % divisor == 0
-       factors << divisor unless divisor == 16
-     end
-   end
- else
-   factors << num
- end
- return factors
-end
-
-def consume_arr(arr)
-  new_arr = []
-  arr.each do |num|
-    new_arr << factors(num)
+  new_arr = arr.map do |num|
+    if num % 2 == 0
+      greatest_factor(num)
+    else
+      num
+    end
   end
-  return new_arr
+  new_arr
 end
+
+# def factors(num)
+# factors = []
+# if num % 2 == 0
+#    (1...num).each do |divisor|
+#      if num % divisor == 0
+#        factors << divisor unless divisor == 16
+#      end
+#    end
+#  else
+#    factors << num
+#  end
+#  return factors
+# end
+
+# Refactor: from factors above.
+def greatest_factor(num)
+  (1...num).reverse_each do |i|
+    if num % i == 0
+      return i
+    end
+  end
+end
+
+# def consume_arr(arr)
+#   new_arr = []
+#   arr.each do |num|
+#     new_arr << factors(num)
+#   end
+#   return new_arr
+# end
 
 print greatest_factor_array([16, 7, 9, 14]) # => [8, 7, 9, 7]
 puts
