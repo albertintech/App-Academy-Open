e49379f0bcfe36ff76727cfa2c81175a58e0203a
Albert Ramos <albertramos.dev@gmail.com>
2021-11-24 15:40:33 -0700

    Add solution

--- advanced_problems/adjacent_sum.rb
+++ advanced_problems/adjacent_sum.rb
@@ -2,8 +2,28 @@
 # Write a method adjacent_sum that takes in an array of numbers and returns a new array containing the sums of adjacent numbers in the original array. See the examples.
 
 def adjacent_sum(arr)
+  new_arr = []
+  idx = 0
 
+  while idx < arr.length - 1
+    new_arr << arr[idx] + arr[idx + 1]
+      idx += 1
+    end
+
+ return new_arr
 end
+
+# def adjacent_sum(arr)
+#   new_arr = []
+#
+#   arr.each_with_index do |ele, i|
+#     if i != arr.length - 1
+#       new_arr << arr[i] + arr[i + 1]
+#     end
+#   end
+#
+#   return new_arr
+# end
 
 print adjacent_sum([3, 7, 2, 11]) #=> [10, 9, 13], because [ 3+7, 7+2, 2+11 ]
 puts
